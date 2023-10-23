95546d8258b912ec5369f55c49e8149d9e5119f1
Ranjitha Sekhar <ranjithakaroline@gmail.com>
2023-10-23 11:06:11 +0200

    updated game

--- game_test.py
+++ game_test.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 
+#WSL1:
+#export LIBGL_ALWAYS_INDIRECT=1
+#export DISPLAY=localhost:0
+
+
 import sys, pygame
 pygame.init()
 
@@ -8,7 +13,7 @@ ran = pygame.image.load("ran.png")
 
 rect = ran.get_rect()
 screen = pygame.display.set_mode((500,500))
-bg = pygame.image.load("bg_small.png")
+bg = pygame.image.load("bg.png")
 
 vel =10 # moves char by 10 pixels for every iteration in the while loop
 
@@ -16,12 +21,14 @@ vel =10 # moves char by 10 pixels for every iteration in the while loop
 while True:
     screen.blit(bg,(0,0))
     pygame.time.delay(100)
-    pygame.display.update()
+    
     
     for event in pygame.event.get():
-        if event.type == pygame.QUIT: 
-        	sys.exit()
-    get_input= pygame.key.get_pressed()
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    
+    get_input = pygame.key.get_pressed()
     if get_input[pygame.K_LEFT]:
     	rect.x -=vel
     if get_input[pygame.K_RIGHT]:
@@ -31,7 +38,7 @@ while True:
     if get_input[pygame.K_DOWN]:
     	rect.y+=vel
 	
-
+    pygame.display.update()
     screen.blit(ran, rect)
     pygame.display.flip()
     
