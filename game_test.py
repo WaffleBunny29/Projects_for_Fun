022e5accc9351255687fa705737c05ed8ad2eff7
Ranjitha Sekhar <ranjithakaroline@gmail.com>
2023-10-23 14:35:54 +0200

    Gravity

--- game_test.py
+++ game_test.py
@@ -14,35 +14,54 @@ screen = pygame.display.set_mode((500,500))
 
 sky = pygame.image.load("sky.png").convert()
 ground = pygame.image.load("ground2.png").convert()
+
 enemy = pygame.image.load("book.png").convert_alpha()
-enemy_rect = enemy.get_rect(midbottom = (400,410))
+enemy_rect = enemy.get_rect(midbottom = (400,400))
+
 text_format = pygame.font.Font('game-played-font/font.ttf',20)
-text = text_format.render('Bachelor of Biomedical Engineering',False,'black')
+text = text_format.render(' Level1',False,'black')
+text_rect = text.get_rect(center = (250,150))
+
 player_ran = pygame.image.load("ran2.png")
-rect = player_ran.get_rect(midbottom = (80,425))
+rect = player_ran.get_rect(midbottom = (80,400))
+gravity = 0
+
 
 vel =15 # moves char by 10 pixels for every iteration in the while loop
 
 
 while True:
-    screen.blit(sky,(0,0))
-    screen.blit(ground,(0,400))
-    screen.blit(text,(75,50))
-    #enemy_rect -=20
-    #if enemy_rect<=-50:
-    #	enemy_rect=550
-    screen.blit(enemy,enemy_rect)
-    screen.blit(player_ran, rect)
-    
-    pygame.time.delay(100)
-    
-    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == pygame.KEYDOWN:
+        	if event.key == pygame.K_SPACE:
+	            print('Jump')
+        if event.type == pygame.KEYUP:
+            print('keyup')
+
+                
+    screen.blit(sky,(0,0))
+    screen.blit(ground,(0,400))
+    pygame.draw.rect(screen,'white',text_rect,0,10)
+    pygame.draw.rect(screen,'gold',text_rect,2,5)
+    screen.blit(text,text_rect)
+    enemy_rect.x -= 20
+    if enemy_rect.x <= -50:
+    	enemy_rect.x = 550
+    screen.blit(enemy,enemy_rect)
     
+    # player
+    gravity += 10
+    rect.y += gravity
+    screen.blit(player_ran, rect)
+    
+    pygame.time.delay(100)
+    
+    # moving player around   
     get_input = pygame.key.get_pressed()
+       	
     if get_input[pygame.K_LEFT]:
     	rect.x -=vel
     if get_input[pygame.K_RIGHT]:
@@ -51,7 +70,10 @@ while True:
     	rect.y-=vel
     if get_input[pygame.K_DOWN]:
     	rect.y+=vel
-	
+    	
+    if rect.colliderect(enemy_rect):
+        print('collision')
+    	
     pygame.display.update()
     clock.tick(50)
     pygame.display.flip()
